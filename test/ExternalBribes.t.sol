3e8efc28694241bf45813f27c90c917cd9fa54d4
dunks411 <54425677+duncan4123@users.noreply.github.com>
2023-02-11 15:45:24 +1100

    fix pendingTank
    deployed to arb goerli again
    
    Comment out require voter UNTIL forge tests can set voter

--- test/ExternalBribes.t.sol
+++ test/ExternalBribes.t.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.8.13;
 
-import './BaseTest.sol';
+import "./BaseTest.sol";
+// import label
 
 contract ExternalBribesTest is BaseTest {
     VotingEscrow escrow;
@@ -12,6 +13,7 @@ contract ExternalBribesTest is BaseTest {
     Gauge gauge;
     InternalBribe bribe;
     ExternalBribe xbribe;
+    PairFactory pairFactory;
 
     function setUp() public {
         vm.warp(block.timestamp + 1 weeks); // put some initial time in
@@ -33,13 +35,40 @@ contract ExternalBribesTest is BaseTest {
         // deployVoter()
         gaugeFactory = new GaugeFactory();
         bribeFactory = new BribeFactory();
-        voter = new Voter(address(escrow), address(factory), address(gaugeFactory), address(bribeFactory));
+        voter = new Voter(
+            address(escrow),
+            address(factory),
+            address(gaugeFactory),
+            address(bribeFactory)
+        );
 
         escrow.setVoter(address(voter));
 
+        // whitelist reward tokens on voter
+
+        voter.whitelist(address(USDC));
+        voter.whitelist(address(FRAX));
+        voter.whitelist(address(DAI));
+        voter.whitelist(address(VELO));
+        voter.whitelist(address(LR));
+
+        // label voter using forge label function
+
+        // function label(address addr, string calldata label) external;
+
+        // label(address(voter), "Voter");
+
+        emit log("dunks");
+
+        // emit log(voter);
+
         // deployMinter()
         distributor = new RewardsDistributor(address(escrow));
-        minter = new Minter(address(voter), address(escrow), address(distributor));
+        minter = new Minter(
+            address(voter),
+            address(escrow),
+            address(distributor)
+        );
         distributor.setDepositor(address(minter));
         VELO.setMinter(address(minter));
         address[] memory tokens = new address[](5);
@@ -51,13 +80,17 @@ contract ExternalBribesTest is BaseTest {
         voter.initialize(tokens, address(minter));
 
         address[] memory claimants = new address[](0);
-        uint[] memory amounts1 = new uint[](0);
+        uint256[] memory amounts1 = new uint256[](0);
         minter.initialize(claimants, amounts1, 0);
 
         // USDC - FRAX stable
         gauge = Gauge(voter.createGauge(address(pair)));
         bribe = InternalBribe(gauge.internal_bribe());
         xbribe = ExternalBribe(gauge.external_bribe());
+
+        // set external bribe on the deployed pair contract
+
+        pair.setExternalBribe(address(xbribe));
 
         // ve
         VELO.approve(address(escrow), TOKEN_1);
@@ -67,7 +100,16 @@ contract ExternalBribesTest is BaseTest {
         escrow.create_lock(TOKEN_1, 4 * 365 * 86400);
         vm.warp(block.timestamp + 1);
         vm.stopPrank();
+
+        // try to set voter on pairFactory
+
+        pairFactory.setVoter(address(voter));
     }
+
+    // this is not working - why? - possibly becuase owner (above directly) is not being used to call setVoter?
+    // call setVoter on pairFactory as owner
+
+    // TestOwner(address(pairFactory)).setVoter(address(voter));
 
     function testCanClaimExternalBribe() public {
         // fwd half a week
