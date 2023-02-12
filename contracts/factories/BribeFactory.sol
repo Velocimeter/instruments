087f7909e8da1bac19f6ebb7398e41d0ed5ee92c
dunks411 <54425677+duncan4123@users.noreply.github.com>
2023-02-13 09:47:56 +1100

    tenderly

--- contracts/factories/BribeFactory.sol
+++ contracts/factories/BribeFactory.sol
@@ -8,24 +8,22 @@ import "contracts/ExternalBribe.sol";
 contract BribeFactory is IBribeFactory {
     address public last_internal_bribe;
     address public last_external_bribe;
+    address public last_nexternal_bribe;
 
-    function createInternalBribe(address[] memory allowedRewards)
-        external
-        returns (address)
-    {
-        last_internal_bribe = address(
-            new InternalBribe(msg.sender, allowedRewards)
-        );
+    function createInternalBribe(address[] memory allowedRewards) external returns (address) {
+        last_internal_bribe = address(new InternalBribe(msg.sender, allowedRewards));
         return last_internal_bribe;
     }
 
-    function createExternalBribe(address[] memory allowedRewards)
-        external
-        returns (address)
-    {
-        last_external_bribe = address(
-            new ExternalBribe(msg.sender, allowedRewards)
-        );
+    function createExternalBribe(address[] memory allowedRewards) external returns (address) {
+        last_external_bribe = address(new ExternalBribe(msg.sender, allowedRewards));
         return last_external_bribe;
+    }
+
+    // adding a new function instead of updating old one so tests still work
+
+    function createNexternalBribe(address[] memory allowedRewards) external returns (address) {
+        last_nexternal_bribe = address(new NexternalBribe(msg.sender, allowedRewards));
+        return last_nexternal_bribe;
     }
 }
