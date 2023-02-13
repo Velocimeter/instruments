bef6b0dbe951e362ae537fd2833db4ef9b241f86
dunks411 <54425677+duncan4123@users.noreply.github.com>
2023-02-13 19:05:27 +1100

    make wrapped bribes deploy automatically
    remove nexternal

--- contracts/factories/BribeFactory.sol
+++ contracts/factories/BribeFactory.sol
@@ -8,7 +8,6 @@ import "contracts/ExternalBribe.sol";
 contract BribeFactory is IBribeFactory {
     address public last_internal_bribe;
     address public last_external_bribe;
-    address public last_nexternal_bribe;
 
     function createInternalBribe(address[] memory allowedRewards) external returns (address) {
         last_internal_bribe = address(new InternalBribe(msg.sender, allowedRewards));
@@ -18,12 +17,5 @@ contract BribeFactory is IBribeFactory {
     function createExternalBribe(address[] memory allowedRewards) external returns (address) {
         last_external_bribe = address(new ExternalBribe(msg.sender, allowedRewards));
         return last_external_bribe;
-    }
-
-    // adding a new function instead of updating old one so tests still work
-
-    function createNexternalBribe(address[] memory allowedRewards) external returns (address) {
-        last_nexternal_bribe = address(new NexternalBribe(msg.sender, allowedRewards));
-        return last_nexternal_bribe;
     }
 }
