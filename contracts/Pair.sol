796a3ea47c1bc0715d8b7de28f650a762e310489
torbik <liker1011@gmail.com>
2023-02-15 19:39:18 +0700

    enable require statemens in pair and all voter related

--- contracts/Pair.sol
+++ contracts/Pair.sol
@@ -39,7 +39,7 @@ contract Pair is IPair {
     address public immutable fees;
     address immutable factory; // explain this? can this be public? can I call this from inside a contract
     address public externalBribe;
-    // address public immutable voter;
+    address public immutable voter;
     address public immutable tank;
     bool public hasGauge;
 
@@ -99,7 +99,7 @@ contract Pair is IPair {
 
     constructor() {
         factory = msg.sender;
-        //voter = PairFactory(msg.sender).voter(); // nice easy way to add the voter :) we already getting this from pair factory tho
+        voter = PairFactory(msg.sender).voter(); // nice easy way to add the voter :) we already getting this from pair factory tho
         tank = PairFactory(msg.sender).tank(); // nice easy way to add the voter :) we already getting this from pair factory tho
         (address _token0, address _token1, bool _stable) = PairFactory(msg.sender).getInitializable(); //wondering why msg.sender is passed here??
         (token0, token1, stable) = (_token0, _token1, _stable);
@@ -139,7 +139,7 @@ contract Pair is IPair {
     }
 
     function setExternalBribe(address _externalBribe) external {
-        // require(msg.sender == voter, "Only voter can set external bribe");
+        require(msg.sender == voter, "Only voter can set external bribe");
         externalBribe = _externalBribe;
         _safeApprove(token0, externalBribe, type(uint256).max);
         _safeApprove(token1, externalBribe, type(uint256).max);
@@ -147,7 +147,7 @@ contract Pair is IPair {
     }
 
     function setHasGauge(bool value) external {
-        // require(msg.sender == voter, "Only voter can set has gauge");
+        require(msg.sender == voter, "Only voter can set has gauge");
         hasGauge = value;
     }
 
