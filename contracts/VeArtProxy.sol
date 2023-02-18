e0bb9806d4a416cd85e471673fe833179b075ff1
dunks411 <54425677+duncan4123@users.noreply.github.com>
2023-02-19 09:25:29 +1100

    art proxy updated

--- contracts/VeArtProxy.sol
+++ contracts/VeArtProxy.sol
@@ -2,17 +2,18 @@ pragma solidity 0.8.13;
 
 import {Base64} from "contracts/libraries/Base64.sol";
 import {IVeArtProxy} from "contracts/interfaces/IVeArtProxy.sol";
+import "openzeppelin-contracts/contracts/utils/math/Math.sol";
 
 contract VeArtProxy is IVeArtProxy {
-    function toString(uint value) internal pure returns (string memory) {
+    function toString(uint256 value) internal pure returns (string memory) {
         // Inspired by OraclizeAPI's implementation - MIT license
         // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
 
         if (value == 0) {
             return "0";
         }
-        uint temp = value;
-        uint digits;
+        uint256 temp = value;
+        uint256 digits;
         while (temp != 0) {
             digits++;
             temp /= 10;
@@ -20,20 +21,52 @@ contract VeArtProxy is IVeArtProxy {
         bytes memory buffer = new bytes(digits);
         while (value != 0) {
             digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint(value % 10)));
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
             value /= 10;
         }
         return string(buffer);
     }
 
-    function _tokenURI(uint _tokenId, uint _balanceOf, uint _locked_end, uint _value) external pure returns (string memory output) {
-        output = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">';
-        output = string(abi.encodePacked(output, "token ", toString(_tokenId), '</text><text x="10" y="40" class="base">'));
-        output = string(abi.encodePacked(output, "balanceOf ", toString(_balanceOf), '</text><text x="10" y="60" class="base">'));
-        output = string(abi.encodePacked(output, "locked_end ", toString(_locked_end), '</text><text x="10" y="80" class="base">'));
-        output = string(abi.encodePacked(output, "value ", toString(_value), '</text></svg>'));
+    function humanize(uint256 value) public pure returns (string memory) {
+        if (value < 1000) {
+            return toString(value);
+        }
+        uint256 exp = Math.min(Math.log10(value) / 3, 5);
+        string[6] memory units = ["k", "M", "B", "T", "Q", "QQ"];
+        uint256 scaled = value / (10 ** (exp * 3));
+        return string(abi.encodePacked(toString(scaled), units[exp - 1]));
+    }
 
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "lock #', toString(_tokenId), '", "description": "Velocimeter locks, can be used to boost gauge yields, vote on token emission, and receive bribes", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
-        output = string(abi.encodePacked('data:application/json;base64,', json));
+    function _tokenURI(uint256 _tokenId, uint256 _balanceOf, uint256 _lockedEnd, uint256 _value)
+        external
+        pure
+        returns (string memory output)
+    {
+        output =
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">';
+        output =
+            string(abi.encodePacked(output, "token ", toString(_tokenId), '</text><text x="10" y="40" class="base">'));
+        output = string(
+            abi.encodePacked(output, "balanceOf ", humanize(_balanceOf), '</text><text x="10" y="60" class="base">')
+        );
+        output = string(
+            abi.encodePacked(output, "lockedEnd ", humanize(_lockedEnd), '</text><text x="10" y="80" class="base">')
+        );
+        output = string(abi.encodePacked(output, "value ", humanize(_value), "</text></svg>"));
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "lock #',
+                        toString(_tokenId),
+                        '", "description": "Velocimeter locks, can be used to boost gauge yields, vote on token emission, and receive bribes", "image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(output)),
+                        '"}'
+                    )
+                )
+            )
+        );
+        output = string(abi.encodePacked("data:application/json;base64,", json));
     }
 }
